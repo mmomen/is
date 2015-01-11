6d53b225b24f08ab9ae59c223798dd4f9b1e30cc
TaeHoon Yoon <thyoon220@gmail.com>
2015-01-11 15:51:40 -0800

    Completed delete functionality
    -Clicking on trash can in option bar activates delete function:
    1. all friends will start wiggling
    2. x mark will appear on right corner of panel title
    3. on click of x will prompt confirmation
    4. on confirm, will delete friend from user's friends list and also delete user from the friend's friends list.

--- client/templates/isers/iser_page.js
+++ client/templates/isers/iser_page.js
@@ -14,5 +14,52 @@ Template.iserPage.events({
   'click .fa-minus-square-o': function(){
     clearSearch();
     Session.set('searchField', "");
+  },
+  'click .fa-trash': function(){
+    var $eachFriend = $('.display-each-friend');
+
+    if ($eachFriend.hasClass('shake')){
+      $eachFriend.removeClass('shake');
+      $('.fa-trash').removeAttr('style');
+      $('.delete-friend-button').remove();
+    }else{
+      $eachFriend.addClass('shake');
+      $('.fa-trash').css('color', 'rgba(40, 75, 130, 0.9)');
+      $('.show-username-username').append("<span class='delete-friend-button'><a href='#' style='text-decoration: none'>&times;</a></span>");
+    }
+  },
+  'click .delete-friend-button': function(){
+    var iser = Isers.findOne({iserId: Meteor.userId()});
+    var username;
+
+    iser.friends.forEach(function(friend){
+      if (friend.friendId === this._id){
+        username = friend.username
+      }
+    }, this);
+
+    var deleteFriend = confirm("Are you sure you want to delete "+ username + "?");
+
+    var friendsFriendArr;
+    if (deleteFriend === true){
+      friendsFriendArr = this.friends.filter(function(friend){
+        if(friend.friendId !== iser._id){
+          return friend
+        }
+      }, this)
+      Meteor.call('updateUsername', this._id, friendsFriendArr);
+    }
+
+    // DELETE FRIEND FROM CURRENT ISER'S FRIENDS LIST
+    var iserFriendsArr;
+    if (deleteFriend === true){
+      friendsArr = iser.friends.filter(function(friend){
+        if (friend.friendId !== this._id){
+          return friend
+        }
+      }, this);
+
+      Meteor.call('updateUsername', iser._id, iserFriendsArr);
+    }
   }
 })
