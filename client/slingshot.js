d36d0bb0ef913d7a25da4d3d9af52067d59be5cd
TaeHoon Yoon <thyoon220@gmail.com>
2015-01-21 20:42:51 -0800

    Added fast-render package
    -Added fastRender: true to the home route and iserpage route to increase speed of load.
    -Added slingshot files for image update. Not yet ready.

---
+++ client/slingshot.js
@@ -0,0 +1,4 @@
+// var uploader = new Slingshot.Upload('myFileUploads');
+// uploader.send(document.getElementById('input').files[0], function(error, downloadUrl){
+//   Meteor.users.update(Meteor.userId(), {$push:{"profile.files": downloadUrl}})
+// });
